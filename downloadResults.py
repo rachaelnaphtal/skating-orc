ae916e5fcc785350981d2008f87c1d22c7587521
Rachael Naphtal <rnaphtal@MacBook-Air-44.local>
2024-12-17 20:45:10 -0500

    Adding base scripts

---
+++ downloadResults.py
@@ -0,0 +1,255 @@
+import asyncio
+from pyppeteer import launch
+import judgingParsing 
+from judgingParsing import FitSheetWrapper
+import requests
+from bs4 import BeautifulSoup
+import xlwt 
+from xlwt import Workbook 
+import pandas as pd
+import xlsxwriter
+
+async def generate_pdf(url, pdf_path):
+    browser = await launch()
+    page = await browser.newPage()
+    
+    await page.goto(url)
+    
+    await page.pdf({'path': pdf_path, 'format': 'A4'})
+    
+    await browser.close()
+
+
+def processEvent(url, eventName, judges, workbook, pdf_number):
+    pdf_path = f"{pdf_folder}{eventName}.pdf"
+    excel_path = excel_folder
+    asyncio.get_event_loop().run_until_complete(generate_pdf(url, pdf_path))
+    return judgingParsing.extract_judge_scores(workbook, pdf_path, excel_path, judges, pdf_number)
+    
+def get_page_contents(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+    }
+
+    page = requests.get(url, headers=headers)
+
+    if page.status_code == 200:
+        return page.text
+
+    return None
+
+def get_urls_and_names(page_contents):
+    soup = BeautifulSoup(page_contents, 'html.parser')
+    links = soup.find_all('a', href=True, string="Final")
+    names = soup.find_all('td', class_='event tRow bRow')
+    return list(set(links)), names
+#processEvent("https://ijs.usfigureskating.org/leaderboard/results/2024/34290/SEGM028.html")
+
+
+def findJudgesNames(soup):
+    alltd = soup.find_all("td")
+    judges = []
+    nextJudge = False
+    for td in alltd:
+        if td.text.count("Judge ") > 0:
+            nextJudge = True
+        elif nextJudge:
+            judges.append(td.text.split(",")[0])
+            nextJudge = False
+    return judges
+
+
+def findResultsDetailUrlAndJudgesNames(base_url, results_Page_link):
+    url = f"{base_url}/{results_Page_link}"
+    page_contents = get_page_contents(url)
+    soup = BeautifulSoup(page_contents, 'html.parser')
+    link = soup.find('a', href=True, string="Judge detail scores")
+    judgesNames = findJudgesNames(soup)
+    return (link["href"], judgesNames)
+
+def scrape(base_url, report_name):
+    url = f"{base_url}/index.asp"
+    page_contents = get_page_contents(url)
+    workbook = xlwt.Workbook()  
+
+    if page_contents:
+        links, names = get_urls_and_names(page_contents)
+        judgeErrors = {}
+        for i in range(len(links)):
+            (resultsLink, judgesNames) = findResultsDetailUrlAndJudgesNames(base_url, links[i]["href"])
+            (event_name, total_errors, allowed_errors) = processEvent(f"{base_url}/{resultsLink}", i, judgesNames, workbook, i)
+            for i in range(len(judgesNames)):
+                judge = judgesNames[i]
+                if judge not in judgeErrors:
+                    judgeErrors[judge] = {}
+                judgeErrors[judge][event_name] = {"Errors": total_errors[i], "Allowed Errors": allowed_errors,  "In Excess": max(total_errors[i]-allowed_errors, 0)}
+        
+        sheet = FitSheetWrapper(workbook.add_sheet("Summary")) 
+        # Specifying style 
+        bold = xlwt.easyxf('font: bold 1') 
+        blue = xlwt.easyxf('pattern: pattern solid, fore_colour blue;')
+        sheet.write(0, 0, "Summary", bold)
+        current_col = 0
+        summary_row=5+ max([len(judgeErrors[judge]) for judge in judgeErrors])
+
+        # Add summary row for all anomalies
+        sheet.write(1, current_col, "Judge Name")
+        sheet.write(1, current_col+1, "# Anomalies")
+        sheet.write(1, current_col+2, "# In Excess")
+        sheet.write(1, current_col+3, "# Events")
+        current_row = 2
+
+       # print (judgeErrors)
+        df_dict = {}
+        for judge in judgeErrors:
+            df_dict[judge] = pd.DataFrame.from_dict(judgeErrors[judge], orient='index')
+        for judge, value in sorted(df_dict.items(), key=lambda kv: kv[1]['Errors'].sum(), reverse=True):
+            sheet.write(current_row, current_col, judge)
+            sheet.write(current_row, current_col+1, int(df_dict[judge]["Errors"].sum()))
+            sheet.write(current_row, current_col+2, int(df_dict[judge]["In Excess"].sum()))
+            sheet.write(current_row, current_col+3, len(judgeErrors[judge]))
+            current_row+=1
+        current_col+=5
+        for judge in judgeErrors:
+            current_row = 1
+            sheet.write(current_row, current_col, judge, bold)
+            current_row+=1
+            sheet.write(current_row, current_col, "Event")
+            sheet.write(current_row, current_col+1, "Anomalies")
+            sheet.write(current_row, current_col+2, "ORC recognized")
+            sheet.write(current_row, current_col+3, "Allowed")
+            sheet.write(current_row, current_col+4, "In Excess")
+            current_row+=1
+
+           
+            for event in judgeErrors[judge]:
+                if event == "Total Errors" or event == "Allowed Errors":
+                    continue
+                sheet.write(current_row, current_col, event.replace("_", " "))
+                num_errors = judgeErrors[judge][event]["Errors"]
+                sheet.write(current_row, current_col+1, num_errors)
+
+                num_allowed = int(judgeErrors[judge][event]["Allowed Errors"])
+                sheet.write(current_row, current_col+3, num_allowed)
+
+                in_excess = int(judgeErrors[judge][event]["In Excess"])
+                sheet.write(current_row, current_col+4, in_excess)
+                current_row+=1
+
+            current_row+=1
+
+            sheet.write(summary_row, current_col, "Total", bold)
+            sheet.write(summary_row, current_col+1, int(df_dict[judge]["Errors"].sum()), bold)
+            sheet.write(summary_row, current_col+4, int(df_dict[judge]["In Excess"].sum()), bold)
+            current_col+=5
+        
+            
+    else:
+        print('Failed to get page contents.')
+
+    excel_path = f"{excel_folder}{report_name}.xls"
+    workbook.save(excel_path) 
+    print ("Finished " + report_name)
+    return df_dict
+
+def create_season_summary(full_report_name="2425Summary"):
+    workbook = xlwt.Workbook()  
+    events = {
+        "Dallas_Classic":"2024/33436",
+        "Cactus_Classic":"2024/34414",
+        "Peach_Open":"2024/33518",
+        "Glacier_Falls":"2024/33519",
+        "Lake_Placid":"2024/33491",
+        "Philadelphia":"2024/33453",
+        "Scott_Hamilton":"2024/33501",
+        "Copper_Cup":"2024/33425",
+        "Cup_of_Colorado":"2024/33507",
+        "Skate_the_Lake":"2024/33520",
+        "MiddleAtlantics":"2024/33515",
+        "SkateSF": "2024/33479", 
+        "Potomac": "2024/33523",
+        #"Providence": "",
+        
+        "Chicagoland":"2024/33497",
+        "JohnSmith":"2024/33451",
+        "Pasadena":"2024/33509",
+        "PNIW":"2024/33489",
+        "Challenge_Cup": "2024/34444",
+        "Skate_Cleveland": "2024/33466",
+        "Austin_Autumn_Classic": "2024/33458",
+        "BostonNQS" : "2024/33526",
+        "Pacifics2425": "2024/34291",
+        "Midwesterns_DanceFinal2425": "2024/34290",
+        "Easterns_PairsFinal2425": "2024/34289",
+    }
+    summary_dict = {}
+
+    for event_name in events:
+        result = scrape(f"https://ijs.usfigureskating.org/leaderboard/results/{events[event_name]}", f"{event_name}")
+        for judge in result:
+            result[judge]["Competition"] = event_name
+            if judge not in summary_dict:
+                summary_dict[judge] = result[judge]
+            else:
+                summary_dict[judge] = pd.concat([summary_dict[judge], result[judge]], axis=0)
+    print_summary_workbook(workbook, summary_dict, full_report_name)
+                
+def print_summary_workbook(workbook, summary_dict, full_report_name):
+    sheet = FitSheetWrapper(workbook.add_sheet("Summary")) 
+    # Specifying style 
+    bold = xlwt.easyxf('font: bold 1') 
+    sheet.write(0, 0, "Summary", bold)
+    current_col = 0
+
+    # Add summary sheet for all anomalies
+    sheet.write(1, current_col, "Judge Name")
+    sheet.write(1, current_col+1, "# Anomalies")
+    sheet.write(1, current_col+2, "# In Excess")
+    sheet.write(1, current_col+3, "# Events")
+    sheet.write(1, current_col+4, "In Excess per event")
+    current_row = 2
+
+    for judge, value in sorted(summary_dict.items(), key=lambda kv: kv[1]['In Excess'].sum(), reverse=True):
+        sheet.write(current_row, current_col, judge)
+        sheet.write(current_row, current_col+1, int(value["Errors"].sum()))
+        sheet.write(current_row, current_col+2, int(value["In Excess"].sum()))
+        num_events = len(value[value["Errors"] >= 0])
+        sheet.write(current_row, current_col+3, num_events)
+        sheet.write(current_row, current_col+4, float(value["In Excess"].sum())/float(num_events))
+        current_row+=1
+
+    excel_path = f"{excel_folder}{full_report_name}.xls"
+    workbook.save(excel_path) 
+    # Add sheets per judge
+    writer = pd.ExcelWriter(f"{excel_folder}{full_report_name}_perJudge.xlsx", engine = 'xlsxwriter')
+    for judge in sorted(summary_dict.keys()):
+        print_sheet_per_judge(writer, judge, summary_dict[judge])
+    writer.close()
+
+def print_sheet_per_judge(writer, judge_name, judge_df):
+    judge_df.rename(columns={"Errors": "Anomalies", "Allowed Errors": "Allowed"})
+    judge_df.to_excel(writer, sheet_name = judge_name)
+    writer.sheets[judge_name].set_column(0, 0, 35)
+    writer.sheets[judge_name].set_column(1, 1, 12)
+    writer.sheets[judge_name].set_column(2, 2, 12)
+    writer.sheets[judge_name].set_column(3, 3, 12)
+    writer.sheets[judge_name].set_column(4, 4, 30)
+
+pdf_folder = "/Users/rnaphtal/Documents/JudgingAnalysis/2425/Results/"  # Update with the correct path
+excel_folder = "/Users/rnaphtal/Documents/JudgingAnalysis/2425/"
+base_url = 'https://ijs.usfigureskating.org/leaderboard/results/2024/34290'
+#report_name = "Mids2024_ORC_Report"
+
+# #Mids 2425
+# pdf_folder = "/Users/rnaphtal/Documents/JudgingAnalysis/2425/Results/"  # Update with the correct path
+# excel_folder = "/Users/rnaphtal/Documents/JudgingAnalysis/2425/"
+# base_url = 'https://ijs.usfigureskating.org/leaderboard/results/2024/34290'
+# report_name = "Mids2425_ORC_Report"
+
+#Easterns/ Pairs Final 2425
+# pdf_folder = "/Users/rnaphtal/Documents/JudgingAnalysis/Easterns/Results/"  # Update with the correct path
+# excel_folder = "/Users/rnaphtal/Documents/JudgingAnalysis/Easterns/"
+# base_url = 'https://ijs.usfigureskating.org/leaderboard/results/2024/34289'
+# report_name = "Easterns_ORC_Report"
+# scrape(base_url, report_name)
+create_season_summary()
---
+++ judgingParsing.py
@@ -0,0 +1,263 @@
+import pdfplumber
+from pypdf import PdfReader
+import pandas as pd
+import re
+
+import asyncio
+from pyppeteer import launch
+import xlwt 
+from xlwt import Workbook 
+import arial10
+
+class FitSheetWrapper(object):
+    """Try to fit columns to max size of any entry.
+    To use, wrap this around a worksheet returned from the 
+    workbook's add_sheet method, like follows:
+
+        sheet = FitSheetWrapper(book.add_sheet(sheet_name))
+
+    The worksheet interface remains the same: this is a drop-in wrapper
+    for auto-sizing columns.
+    """
+    def __init__(self, sheet):
+        self.sheet = sheet
+        self.widths = dict()
+
+    def write(self, r, c, label='', *args, **kwargs):
+        self.sheet.write(r, c, label, *args, **kwargs)
+        width = int(arial10.fitwidth(label))
+        if width > self.widths.get(c, 0):
+            self.widths[c] = width
+            self.sheet.col(c).width = width
+
+    def __getattr__(self, attr):
+        return getattr(self.sheet, attr)
+
+async def generate_pdf(url, pdf_path, judges):
+    browser = await launch()
+    page = await browser.newPage()
+    
+    await page.goto(url)
+    
+    await page.pdf({'path': pdf_path, 'format': 'A4'})
+    
+    await browser.close()
+
+def extract_judge_scores(workbook, pdf_path, base_excel_path,judges, pdf_number):
+    # Initialize list for storing extracted data
+    elements_per_skater = {}
+    pcs_per_skater = {}
+    skater_details = {}
+    event_name=""
+
+    # Open the PDF file
+    with PdfReader(pdf_path) as pdf:
+    #with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            if not text:
+                return
+    
+        #if "JUDGES DETAILS PER SKATER" in text:
+            text = text.replace("\xa0", "")
+            
+            # Split text into lines
+            lines = text.split("\n")
+            if event_name == "":
+                event_name=lines[2].replace("/","").replace(" ", "_").replace("__", "_").replace("-", "_") 
+                event_name = event_name.split("/")[0]
+                event_name = event_name.split(":")[0]
+
+            current_skater = None
+            for line in lines:
+                # Match skater's name section
+                skater_match = re.match(r"^(\d+)\s+([A-Za-z\(\)\/\-\s]+),?([A-Za-z\(\)\-\s]+)?\s+([\d]{1,3}\.[\d\.]{2})\s*([\d\.]+)\s+([\d\.]+)\s+([\d\.]+)$", line)
+                if skater_match:
+                    skater_name = skater_match.group(2).strip()
+                    technical_score = skater_match.group(5)
+                    current_skater = skater_name
+                    if current_skater not in elements_per_skater:
+                        elements_per_skater[current_skater] = []
+                        pcs_per_skater[current_skater] = []
+                        skater_details[current_skater] = technical_score
+                    continue
+
+                # Match elements and judge scores
+                element_match = re.match(r"^(\d+)(\s)*([\w\+\.\*<>!]+(?:\s+\w+)?)\s+(?:([F*<!>q]+)\s+)?(-?[\d\.]+x?)\s+(?:([F\*x]+)\s+)?(-?[\d\.]+)\s+(-?(?:-?\d\s+){3,9})\s*([\d\.]+\s)?([\d\.]+)", line)
+                pcs_match = re.match(r"^^\s*(Timing|Presentation|Skating\sSkills|Composition)\s+([\d\.]+)\s+((?:[\d\.]{4}\s*){3,9})([\d\.]+)", line)
+                if current_skater and element_match:
+                   # element_number = int(element_match.group(1))
+                    element_name = element_match.group(3)
+                    judges_scores = list(map(float, element_match.group(8).split()))
+                    total_score = float(element_match.group(10))
+                    #scores = list(map(int, element_match.group(2).split()))
+                    elements_per_skater[current_skater].append({
+                        "Element": element_name,
+                        "Scores": judges_scores,
+                        "Value": total_score
+                    })
+                elif current_skater and pcs_match:
+                    component = pcs_match.group(1)
+                    no_spaces = pcs_match.group(3).replace(" ","")
+                    scores = [no_spaces[i:i+4] for i in range(0, len(no_spaces), 4)]
+                    judges_scores = list(map(float, scores))
+                    pcs_per_skater[current_skater].append({
+                        "Component": component,
+                        "Scores": judges_scores
+                    })
+      
+
+        for skater in elements_per_skater:
+            foundElements = round(sum([x["Value"] for x in elements_per_skater[skater]]), 2)
+            expected = float(skater_details[skater])
+            if (foundElements != expected):
+                print (f"Elements for skater {skater} do not match. Expected TES:{expected}, Sum of elements:{foundElements} {pdf_path}")
+                #print (f"Elements for {skater}: {[f"{x["Element"]} ({x["Value"]})" for x in skater_scores[skater]]}")
+            #else:
+                #print (f"Elements for skater {skater} do match.")
+            pcs = pcs_per_skater[skater]
+            if (len(pcs) != 3):
+                print(f"Components missing for skater {skater} {pdf_path}")
+        
+        element_errors = findElementDeviations(elements_per_skater, judges)
+        pcs_errors = findPCSDeviations(pcs_per_skater, judges)
+        total_errors = count_total_errors_per_judge(judges, element_errors, pcs_errors)
+
+        printToExcel(workbook, base_excel_path, event_name, judges, element_errors, pcs_errors, total_errors, pdf_number)
+        return (event_name, total_errors, get_allowed_errors(len(skater_details)))
+   
+    
+def get_allowed_errors(num_skaters : int):
+    if num_skaters <= 10:
+        return 1
+    elif num_skaters <=20:
+        return 2
+    return 3
+
+def findElementDeviations(skater_scores, judges):
+    errors = []
+    for skater in skater_scores:
+        for element in skater_scores[skater]:
+            allScores = element["Scores"]
+            avg = sum(allScores)/len(allScores)
+            judgeNumber = 1
+            for judgeNumber in range(1,len(allScores)+1):
+                deviation = abs(avg-allScores[judgeNumber-1])
+                if (deviation> 2):
+                    #print (f"Deviation found for judge {judgeNumber} on skater {skater}, element {element["Element"]}")
+                    errors.append({
+                        "Skater": skater,
+                        "Element": element["Element"],
+                        "Judge Number": judgeNumber,
+                        "Judge Name": judges[judgeNumber-1],
+                        "Judge Score": allScores[judgeNumber-1],
+                        "Panel Average":avg,
+                        "Deviation": deviation
+                        })
+    return errors
+
+def findPCSDeviations(skater_scores, judges):
+    errors = []
+    for skater in skater_scores:
+        deviation_points = [float(0)]*(len(judges)+1)
+        for component in skater_scores[skater]:
+            allScores = component["Scores"]
+            avg = sum(allScores)/len(allScores)
+            for judgeNumber in range(1,len(allScores)+1):
+                deviation = abs(avg-allScores[judgeNumber-1])
+                deviation_points[judgeNumber]= deviation_points[judgeNumber]+ deviation
+
+        for judgeNumber in range(1,len(allScores)+1):
+            if (deviation_points[judgeNumber] > 4.5):
+                print (f"Deviation found for judge {judgeNumber} on skater {skater}")
+
+                errors.append({
+                    "Skater": skater,
+                    "Judge Number": judgeNumber,
+                    "Judge Name": judges[judgeNumber-1],
+                    "Judge Score": "",
+                    "Deviation": deviation_points[judgeNumber]
+                    })
+    return errors
+
+def count_total_errors_per_judge(judges, element_errors, pcs_errors) -> list:
+    errors_list = [0]*len(judges)
+    for error in element_errors:
+        judgeNumber = error["Judge Number"]
+        errors_list[judgeNumber-1] = errors_list[judgeNumber-1]+ 1
+    for error in pcs_errors:
+        judgeNumber = error["Judge Number"]
+        errors_list[judgeNumber-1] = errors_list[judgeNumber-1]+ 1
+    return errors_list
+
+def printToExcel(workbook, base_excel_path, event_name, judges, element_errors, pcs_errors, total_errors, pdf_number):
+    sheet_name = event_name
+    with_el_match = re.match(r"(\d{1,3})_(\d{1,3}_)?(.*)", sheet_name)
+    if with_el_match:
+        sheet_name=with_el_match.group(3)
+    sheet_name = f"{sheet_name[0:min(len(sheet_name),26)]}{pdf_number}"
+    sheet = FitSheetWrapper(workbook.add_sheet(sheet_name)) 
+    # Specifying style 
+    bold = xlwt.easyxf('font: bold 1') 
+    sheet.write(0, 0, event_name, bold) 
+    # Headers
+    sheet.write(3, 0, "Judge")
+    sheet.write(3, 1, "Judge Score")
+    sheet.write(3, 2, "Deviation From Panel Average")
+    sheet.write(3, 3, "Skater(s)/Couple(s)")
+    #sheet.write(3, 4, "Element #")
+    sheet.write(3, 5, "Element Name")
+    sheet.write(3, 6, "ORC Comments")
+    sheet.write(3, 7, "ORC Error?")
+    sheet.write(4, 0, "A. RANGES OF GOE", bold)
+
+    current_row = 5
+    for error in element_errors:
+        sheet.write(current_row, 0, f"J{error["Judge Number"]}- {error["Judge Name"]}")
+        sheet.write(current_row, 1, error["Judge Score"])
+        sheet.write(current_row, 2, error["Deviation"])
+        sheet.write(current_row, 3, error["Skater"])
+        sheet.write(current_row, 5, error["Element"])
+        current_row=current_row+1
+
+    current_row+=1
+    sheet.write(current_row, 0, "B. RANGES OF PROGRAM COMPONENTS", bold)
+    current_row+=1
+    for error in pcs_errors:
+        sheet.write(current_row, 0, str(f"J{error["Judge Number"]}- {error["Judge Name"]}"))
+        sheet.write(current_row, 1, error["Judge Score"])
+        sheet.write(current_row, 2, str(error["Deviation"]))
+        sheet.write(current_row, 3, error["Skater"])
+        current_row=current_row+1
+
+    current_row+=2
+    sheet.write(current_row, 0, "Judge", bold)
+    sheet.write(current_row, 1, "# of Anomalies", bold)
+    sheet.write(current_row, 2, "ORC Recognized", bold)
+    current_row+=1
+
+    for i in range(len(judges)):
+        sheet.write(current_row, 0, judges[i])
+        sheet.write(current_row, 1, total_errors[i])
+        current_row+=1
+    # # Convert extracted data to a DataFrame
+    # df = pd.DataFrame.from_records(errors)
+
+    # excel_path = f"{base_excel_path}{event_name.replace("/","")}.xls"
+    # workbook.save(excel_path) 
+    # # Save the DataFrame to an Excel file
+    # df.to_excel(excel_path, index=False)
+    #print(f"Judge scores successfully saved to {excel_path}")
+    print (f"Processed {event_name}")
+
+
+
+# Specify paths for the input PDF and output Excel file
+pdf_path = "/Users/rnaphtal/Documents/JudgingAnalysis/2425/Results/19.pdf"  # Update with the correct path
+excel_path = "/Users/rnaphtal/Documents/JudgingAnalysis/Easterns/"
+
+# # # Extract judge scores
+# workbook = xlwt.Workbook()  
+# extract_judge_scores(workbook, pdf_path, excel_path, ["a", "a", "a", "a", "a", "a", "a", "b", "b"])
+# excel_path = f"{excel_path}DeviationsReport2.xls"
+# workbook.save(excel_path) 
