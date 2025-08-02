ec953b5b3c6ecc0cb180a74e1eb9c0dbeb74ccd9
Rachael Naphtal <rnaphtal@gmail.com>
2025-08-02 16:28:29 -0400

    quoting

--- setup.sh
+++ setup.sh
@@ -9,11 +9,11 @@ enableCORS = false
 
 [connections.postgresql]
 dialect = "postgresql"
-host = "$DB_HOST"
-port = "$DB_PORT"
-database = "$DB_NAME"
-username = "$DB_USERNAME"
-password = "$DB_PASSWORD"
+host = \"$DB_HOST\"
+port = \"$DB_PORT\"
+database = \"$DB_NAME\"
+username = \"$DB_USERNAME\"
+password = \"$DB_PASSWORD\"
 
 " > ~/.streamlit/config.toml
 
