ba582cf2ce34735b9f6b74b3f1d8b0a07bad7a77
Rachael Naphtal <rnaphtal@gmail.com>
2025-08-02 16:17:20 -0400

    more setup debugging

--- setup.sh
+++ setup.sh
@@ -16,3 +16,19 @@ username = $DB_USERNAME
 password = $DB_PASSWORD
 
 " > ~/.streamlit/config.toml
+
+echo "
+[server]
+headless = true
+port = $PORT
+enableCORS = false
+
+[connections.postgresql]
+dialect = "postgresql"
+host = $DB_HOST
+port = $DB_PORT
+database = $DB_NAME
+username = $DB_USERNAME
+password = $DB_PASSWORD
+
+"
