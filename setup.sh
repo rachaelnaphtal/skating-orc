a69a543d95eedab9056b942830034baa4fd4dfe2
Rachael Naphtal <rnaphtal@gmail.com>
2025-08-02 15:24:43 -0400

    Add database config

--- setup.sh
+++ setup.sh
@@ -5,4 +5,12 @@ headless = true
 port = $PORT
 enableCORS = false
 
+[connections.postgresql]
+dialect = "postgresql"
+host = $DB_HOST
+port = $DB_PORT
+database = $DB_NAME
+username = $DB_USERNAME
+password = $DB_PASSWORD
+
 " > ~/.streamlit/config.toml
